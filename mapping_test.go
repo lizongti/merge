21ff2d66554667a59bd5c0800acce65d43cbf5e9
lizongti <lzongti@gmail.com>
2022-09-27 16:32:29 +0800

    Add test merge

--- mapping_test.go
+++ mapping_test.go
@@ -19,10 +19,34 @@ func TestValue(t *testing.T) {
 }
 
 func TestPointer(t *testing.T) {
-	var a int = 10
-	var b int = 1
-	ret := merge.MustMerge(&a, &b,
+	var (
+		a  int  = 10
+		b  int  = 1
+		pa *int = &a
+		pb *int = &b
+	)
+
+	assert.Equal(t, &b, merge.MustMerge(&a, &b,
+		merge.WithResolver(merge.ResolverNone),
+		merge.WithCondition(merge.ConditionCoverAll)).(*int))
+
+	assert.Equal(t, 1, *merge.MustMerge(&a, &b,
 		merge.WithResolver(merge.ResolverBoth),
-		merge.WithCondition(merge.ConditionCoverAll))
-	assert.Equal(t, 1, *ret.(*int))
+		merge.WithCondition(merge.ConditionCoverAll)).(*int))
+
+	assert.Equal(t, 1, **merge.MustMerge(&pa, &pb,
+		merge.WithResolver(merge.ResolverDeepBoth),
+		merge.WithCondition(merge.ConditionCoverAll)).(**int))
+
+	assert.Equal(t, 1, *merge.MustMerge(&a, b,
+		merge.WithResolver(merge.ResolverSingle),
+		merge.WithCondition(merge.ConditionCoverAll)).(*int))
+
+	assert.Equal(t, 1, *merge.MustMerge(&a, &pb,
+		merge.WithResolver(merge.ResolverDeepSingle),
+		merge.WithCondition(merge.ConditionCoverAll)).(*int))
+
+	assert.Equal(t, 1, merge.MustMerge(a, &pb,
+		merge.WithResolver(merge.ResolverDeepSingle),
+		merge.WithCondition(merge.ConditionCoverAll)).(int))
 }
